7abc626e242ae4402409f4bf1abccde3dd07cf2c
Eid Hachem <eidhachem1@gmail.com>
2022-04-05 11:58:17 +0300

    fix JS linter errors

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-const hamburger = document.querySelector(".ham");  
-const navSub = document.querySelector(".nav-sub"); 
-const title = document.querySelector(".title"); 
+const hamburger = document.querySelector('.ham');
+const navSub = document.querySelector('.nav-sub');
+const title = document.querySelector('.title');
 
-function openCloseMenu () {
-  hamburger.classList.toggle("change"); 
-  navSub.classList.toggle("nav-change");
-  title.classList.toggle("title-change");
+function openCloseMenu() {
+  hamburger.classList.toggle('change');
+  navSub.classList.toggle('nav-change');
+  title.classList.toggle('title-change');
 }
 
-hamburger.addEventListener("click", openCloseMenu);
+hamburger.addEventListener('click', openCloseMenu);
 
-const scrollToPortfolio = document.querySelector(".nav-portfolio");
+const scrollToPortfolio = document.querySelector('.nav-portfolio');
 
-function scrollToSectionPortfolio () {
+function scrollToSectionPortfolio() {
   openCloseMenu();
 }
 
-scrollToPortfolio.addEventListener("click", scrollToSectionPortfolio);
+scrollToPortfolio.addEventListener('click', scrollToSectionPortfolio);
 
-const scrollToAbout = document.querySelector(".nav-about");
+const scrollToAbout = document.querySelector('.nav-about');
 
-function scrollToSectionContact () {
+function scrollToSectionContact() {
   openCloseMenu();
 }
 
-scrollToAbout.addEventListener("click", scrollToSectionContact);
+scrollToAbout.addEventListener('click', scrollToSectionContact);
 
-const scrollToContact = document.querySelector(".nav-contact");
+const scrollToContact = document.querySelector('.nav-contact');
 
-function scrollToSectionAbout () {
-  openCloseMenu();  
+function scrollToSectionAbout() {
+  openCloseMenu();
 }
 
-scrollToContact.addEventListener("click", scrollToSectionAbout);
+scrollToContact.addEventListener('click', scrollToSectionAbout);
